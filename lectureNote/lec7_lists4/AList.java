14f050200cca33eea9f6b5ae542ece3f5368e03c
MINGNING <2664135882@qq.com>
2025-02-26 12:10:26 +0800

    Pineapple

--- lectureNote/lec7_lists4/AList.java
+++ lectureNote/lec7_lists4/AList.java
@@ -1,35 +1,56 @@
 package lec7_lists4;
 
-public class AList {
-    private int[] items;
+public class AList<Pineapple> {
+    private Pineapple[] items;
     private int size;
 
     // The next item we want to add, will go in position...size!
     // The last item in the list is at position...size - 1.
     /** Creates an empty list. */
     public AList() {
-        items = new int[100];
+        items = (Pineapple[]) new Object[100];
         size = 0;
     }
 
+    /** Resizes the underlying array to the target capacity. */
+    private void resize(int capacity) {
+        Pineapple[] a = (Pineapple[]) new Object[capacity];
+        System.arraycopy(items,0,a,0,size);
+        items = a;
+    }
+
     /** Insert x into the back of the list. */
-    public void addLast(int x) {
+    public void addLast(Pineapple x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+
         items[size] = x;
         size += 1;
     }
 
     /** Returns the item from the back of the list. */
-    public int getLast() {
+    public Pineapple getLast() {
         return items[size - 1];
     }
 
     /** Gets the ith item in the list (0 is the front). */
-    public int get(int i) {
+    public Pineapple get(int i) {
         return items[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
         return size;
+    }
+
+    /**
+     * Deletes item from back of the list and returns deleted item.
+     */
+    public Pineapple removeLast() {
+        Pineapple returnItem = getLast();
+        items[size - 1] = null;
+        size -= 1;
+        return returnItem;
     }
 }
